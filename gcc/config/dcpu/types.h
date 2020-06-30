4dd75feb19074d09b659ca7ae8b10292e8c01bd8
Tyler Kuhn <contact@tyknet.tk>
2020-06-30 01:38:57 -0400

    Forced All Types to 16-Bit
    Forced all types and alignments to 16-bit (for now?)

--- gcc/config/dcpu/types.h
+++ gcc/config/dcpu/types.h
@@ -9,23 +9,34 @@
 
 //Define basic C types
 #define CHAR_TYPE_SIZE 16
+#define BOOL_TYPE_SIZE 16
 #define SHORT_TYPE_SIZE 16
 #define INT_TYPE_SIZE 16
-#define LONG_TYPE_SIZE 32
-#define LONG_LONG_TYPE_SIZE 64
+#define LONG_TYPE_SIZE 16
+#define LONG_LONG_TYPE_SIZE 16
 
-#define WCHAR_TYPE_SIZE 16
 #define WCHAR_TYPE "unsigned int"
+#define WCHAR_TYPE_SIZE 16
+
+#define WINT_TYPE "unsigned int"
+
+#define INTMAX_TYPE "int"
+#define UINTMAX_TYPE "int"
 
 #define SIZE_TYPE "unsigned int"
 
+#define POINTER_SIZE 16
 #define PTR_TYPE "unsigned int"
 #define PTRDIFF_TYPE "int"
 
 //Define extra type information
 #define DEFAULT_SIGNED_CHAR 0
 
-//Define floating point sizes despite the fact we lack that capability
-#define FLOAT_TYPE_SIZE 32
-#define DOUBLE_TYPE_SIZE 64
-#define LONG_DOUBLE_TYPE_SIZE 64
+//Define floating point sizes despite no support
+#define FLOAT_TYPE_SIZE 16
+#define DOUBLE_TYPE_SIZE 16
+#define LONG_DOUBLE_TYPE_SIZE 16
+#define WIDEST_HARDWARE_FP_SIZE 16
+
+#define BIGGEST_ALIGNMENT 16
+#define FASTEST_ALIGNMENT 16
